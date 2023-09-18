005df917cf97db25f6b25d5c056f04943194ac86
Joguisa <jsglln@hotmail.com>
2023-09-17 23:35:38 -0500

    carpeta estilos 80%

---
+++ environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+    name: 'default',
+    firebase: {
+        config : {
+            apiKey: "AIzaSyA2-to0rUS2u1bex0oB2z774P3m9JaDWEs",
+            authDomain: "ecommerce-be442.firebaseapp.com",
+            projectId: "ecommerce-be442",
+            storageBucket: "ecommerce-be442.appspot.com",
+            messagingSenderId: "953545448004",
+            appId: "1:953545448004:web:363c8ba2b270e3ade0ba11"
+        }
+    }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ecommerce';
+
+  constructor( private fs: AngularFirestore) { }
+
+  ngOnInit(): void {
+    this.fs.collection('test').snapshotChanges().subscribe( personas => {
+      console.log(personas.map( x => x.payload.doc.data()));
+    })
+  }
+
 }
